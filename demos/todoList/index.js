c587aa667eef17459d4bf51964e47d05ec20b3d0
novaline <novaline@qq.com>
2016-06-07 20:26:58 +0800

    no message

--- demos/todoList/index.js
+++ demos/todoList/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const qs = require('querystring');
 
 const port = 3000;
 let items = [];
@@ -25,13 +26,13 @@ server.listen(port, () => {
 });
 
 const show = (res) => {
-
-    const itemTpl = items.map((item) => {
+    //这里不能用const定义itemTpl
+    let itemTpl = items.map((item) => {
         return '<li>'+ item + '</li>';
     }).join('');
     let html = "<html>" +
             "<head>" +
-                "<title>TodoList</title>" + 
+                "<title>TodoList</title>" +
             "</head>" +
             "<body>" +
                 "<h1>Todo List</h1>" +
@@ -57,13 +58,28 @@ const show = (res) => {
 };
 
 const add = (req, res) => {
-
+    let body = '';
+    req.setEncoding('utf8');
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        console.log('body', body)
+        let obj = qs.parse(body);
+        console.log('obj',obj);
+        items.push(obj.item);
+        show(res);
+    })
 };
 
 const badRequest = (res) => {
-
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bad Request');
 };
 
 const NotFound = (res) => {
-
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
 };
