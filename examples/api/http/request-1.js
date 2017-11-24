84cb242c84826bd40fadddeb1abaaf43c50f8acc
novaline <novaline@qq.com>
2017-11-24 18:50:05 +0800

    添加动态向es6 class添加方法的例子

--- examples/api/http/request-1.js
+++ examples/api/http/request-1.js
@@ -4,48 +4,48 @@ const querystring = require('querystring');
 
 const port = 8080;
 const requestBook = (q, number = 1, cb) => {
-    // const params = querystring.stringify({
-    //     q,
-    //     number
-    // })
-    const options = {
-        protocol: 'http:',
-        hostname: 'it-ebooks-api.info',
-        method: 'GET',
-        path: '/v1/search/'+ q + '/page/' + number
-    };
+  // const params = querystring.stringify({
+  //     q,
+  //     number
+  // })
+  const options = {
+    protocol: 'http:',
+    hostname: 'it-ebooks-api.info',
+    method: 'GET',
+    path: '/v1/search/' + q + '/page/' + number
+  };
 
-    const req = http.request(options, (res) => {
-        let data = '';
-        console.log(`status: ${res.statusCode}\n`);
-        console.log(`headers: ${util.inspect(res.headers)}\n`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            data += chunk;
-        });
-        res.on('end', () => {
-            data = JSON.parse(data);
-            console.log(`body: ${util.inspect(data, {depth: null})}\n`);
-            // console.log('body: %j', JSON.parse(data));
-            cb(data);
-        });
-    })
-
-    req.on('error', (e) => {
-        console.log(`problem with request: ${e.message}\n`);
+  const req = http.request(options, (res) => {
+    let data = '';
+    console.log(`status: ${res.statusCode}\n`);
+    console.log(`headers: ${util.inspect(res.headers)}\n`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      data += chunk;
     });
+    res.on('end', () => {
+      data = JSON.parse(data);
+      console.log(`body: ${util.inspect(data, { depth: null })}\n`);
+      // console.log('body: %j', JSON.parse(data));
+      cb(data);
+    });
+  })
 
-    req.end();
+  req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}\n`);
+  });
+
+  req.end();
 };
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method.toLowerCase();
+  const url = req.url;
+  const method = req.method.toLowerCase();
 
-    switch (method) {
-        case 'get':
-            if(url === '/') {
-                const html = `
+  switch (method) {
+    case 'get':
+      if (url === '/') {
+        const html = `
                     <html>
                         <header>
                             <title>Search book</title>
@@ -60,39 +60,39 @@ const server = http.createServer((req, res) => {
                         </body>
                     </html>
                 `;
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html;charset=utf-8');
-                res.end(html);
-            } else {
-                res.statusCode = 404;
-                res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-                res.end('404,找不到页面～');
-            }
-            break;
-        case 'post':
-            if('/search' === url) {
-                //这里如果声明let body;则最后body += chunk结果是undefined
-                let body = '';
-                req.setEncoding('utf8');
-                req.on('data', (chunk) => {
-                    body += chunk;
-                });
-                req.on('end', () => {
-                    body = querystring.parse(body);
-                    console.log(`bookname is: ${body.bookname}`);
-                    requestBook(body.bookname, 1, (data) => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json; charset=utf-8');
-                        res.end(JSON.stringify(data));
-                    });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html;charset=utf-8');
+        res.end(html);
+      } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end('404,找不到页面～');
+      }
+      break;
+    case 'post':
+      if ('/search' === url) {
+        //这里如果声明let body;则最后body += chunk结果是undefined
+        let body = '';
+        req.setEncoding('utf8');
+        req.on('data', (chunk) => {
+          body += chunk;
+        });
+        req.on('end', () => {
+          body = querystring.parse(body);
+          console.log(`bookname is: ${body.bookname}`);
+          requestBook(body.bookname, 1, (data) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json; charset=utf-8');
+            res.end(JSON.stringify(data));
+          });
 
-                });
-            }
-            break;
-    }
+        });
+      }
+      break;
+  }
 
 });
 
 server.listen(port, () => {
-    console.log(`Server is listen on port: ${port}`)
+  console.log(`Server is listen on port: ${port}`)
 });
---
+++ examples/dynamic-add-functions-to-es6-classes/index.js
@@ -0,0 +1,27 @@
+class Test {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayName() {
+    console.log(this.name);
+  }
+
+  sayAge() {
+    console.log(this.age);
+  }
+}
+
+const coin = () => Math.random() < 0.5;
+
+if (coin()) {
+  Test.prototype.sayHello = function () {
+    console.log('hello');
+  }
+}
+
+const m = new Test();
+m.sayHello();
+
+
